3281c02e3b149b364ba9de939f341492d97622de
damercadoo <damercadoo@unal.edu.co>
2020-05-01 12:45:42 -0500

    add event module

---
+++ eventModule/app.js
@@ -0,0 +1,8 @@
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+//register a listener
+emitter.on("messageLogged", function () {
+  console.log("listener called");
+});
+//raise event
+emitter.emit("messageLogged");

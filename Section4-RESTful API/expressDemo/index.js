c546446dcd2a280ed68b31cbb19b5a17a551ec20
damercadoo <damercadoo@unal.edu.co>
2020-05-05 18:04:07 -0500

    add delete to the crud

--- Section4-RESTful API/expressDemo/index.js
+++ Section4-RESTful API/expressDemo/index.js
@@ -25,17 +25,15 @@ app.get("/api/courses", (req, res) => {
 app.get("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) {
-    res.status(404).send("the course with the given id was not found");
-  } else {
-    res.send(course);
+    return res.status(404).send("the course with the given id was not found");
   }
+  res.send(course);
 });
 
 app.post("/api/courses", (req, res) => {
   const { error } = validateCourse(req.body);
   if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
+    return res.status(400).send(error.details[0].message);
   }
 
   const course = {
@@ -49,16 +47,25 @@ app.post("/api/courses", (req, res) => {
 app.put("/api/courses/:id", (req, res) => {
   const course = courses.find((c) => c.id === parseInt(req.params.id));
   if (!course) {
-    res.status(404).send("the course with the given id was not found");
+    return res.status(404).send("the course with the given id was not found");
   }
 
   const { error } = validateCourse(req.body);
   if (error) {
-    res.status(400).send(error.details[0].message);
-    return;
+    return res.status(400).send(error.details[0].message);
   }
 
   course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    return res.status(404).send("the course with the given id was not found");
+  }
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
   res.send(course);
 });
 
